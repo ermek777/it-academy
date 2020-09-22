def70da03fbb666ca2107a6693384c08c4d36c09
Ermek07 <ermekxt22@gmail.com>
2020-09-23 02:21:53 +0600

    my first commit

---
+++ taskmanager/firstapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('', views.main_page, name='main-page'),
+   path('<int:pk>/', views.task_detail, name='task-detail'),
+   path('create/', views.create_task, name='create-task'),
+   path('delete/<int:pk>/', views.delete_task, name='delete-task'),
+   path('tasks/<int:pk>/change/', views.change_task, name='change-task'),
+]
